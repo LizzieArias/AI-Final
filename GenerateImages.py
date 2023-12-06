04e55c5d67d93c45ddc086374a103efa46d6276b
LizzieArias <lariasdejesus@plu.edu>
2023-12-05 21:17:41 -0800

    Set up with API

---
+++ GenerateImages.py
@@ -0,0 +1,25 @@
+import openai 
+import requests
+from PIL import Image
+import React
+
+
+openai.api_key= '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'
+
+# def generateImage(text):
+#     inputText = openai.Image.create(
+#         prompt = text,
+
+#         n = 1,
+
+#         size = "50x50",
+#     )
+#     return inputText["data"][0]["url"]
+
+# # Prompt 
+# text = "Hello Kitty"
+
+# result = generateImage(text)
+
+
+
